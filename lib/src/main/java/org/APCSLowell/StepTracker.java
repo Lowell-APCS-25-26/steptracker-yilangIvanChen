f03ad777eab6bb2c05c6fb09331c3f9622231718
yilangIvanChen <yichen5@s.sfusd.edu>
2026-04-21 16:19:42 +0000

    skibidi

--- lib/src/main/java/org/APCSLowell/StepTracker.java
+++ lib/src/main/java/org/APCSLowell/StepTracker.java
@@ -1,8 +1,25 @@
 package org.APCSLowell;
 
 public class StepTracker {
-    public StepTracker(int activityThreshold) {}
-    void addDailySteps(int stepCount) {}
-    int activeDays() {}
-    double averageSteps() {}
+    private int minSteps, totSteps, actDays, totDays;
+    public StepTracker(int activityThreshold) {
+        minSteps = activityThreshold;
+        totSteps = 0;
+        actDays = 0;
+        totDays = 0;
+    }
+    public void addDailySteps(int stepCount) {
+        if (stepCount >= minSteps)
+            actDays++;
+        totDays++;
+        totSteps += stepCount;
+    }
+    public int activeDays() {
+        return actDays;
+    }
+    public double averageSteps() {
+        if (totDays == 0)
+            return 0;
+        return (double) totSteps/totDays;
+    }
 }
